6abe8dee1bb08cdb3674f5dba4b8502f1d7a7d1c
Fred Dubois <169247+duboisf@users.noreply.github.com>
2019-10-12 14:27:32 -0400

    Updates

---
+++ precache-manifest.dcb55cf8910b14ada1d494ae6bd330f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae0f39476f60a336c9192bb1ae42a7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc737b97f932f0278fd1",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "838275b97fbb9fbf31b8",
+    "url": "/static/js/2.2376b070.chunk.js"
+  },
+  {
+    "revision": "cc737b97f932f0278fd1",
+    "url": "/static/js/main.6b8357d7.chunk.js"
+  },
+  {
+    "revision": "8d76f2e404948bb11592",
+    "url": "/static/js/runtime-main.5e39e6f9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
